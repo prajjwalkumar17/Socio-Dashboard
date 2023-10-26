18109bcb462fe01321aaedb7fc6f941d0cf63193
prajjwalkumar17 <prajjwal.kumar@juspay.in>
2023-10-26 19:14:03 +0530

    feat(admin-ui): added the language in i18

---
+++ target/debug/Socio-Dashboard.d
@@ -0,0 +1,1 @@
+/Users/prajjwal.kumar/test/Socio-Dashboard/target/debug/Socio-Dashboard: /Users/prajjwal.kumar/test/Socio-Dashboard/src/main.rs
---
+++ target/debug/deps/Socio_Dashboard-1abb4c811c08938b.d
@@ -0,0 +1,5 @@
+/Users/prajjwal.kumar/test/Socio-Dashboard/target/debug/deps/Socio_Dashboard-1abb4c811c08938b: src/main.rs
+
+/Users/prajjwal.kumar/test/Socio-Dashboard/target/debug/deps/Socio_Dashboard-1abb4c811c08938b.d: src/main.rs
+
+src/main.rs:
